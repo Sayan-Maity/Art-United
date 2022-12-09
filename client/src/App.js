d941b6f03c0e9b9bea6560d523a7102d55e5a022
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-09 06:15:56 +0530

    #updted the routes and the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
-import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/icons-material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
         <Routes>
-          <Route path="/" element={<LoginPage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
+      </ThemeProvider>
       </BrowserRouter>
-      
     </div>
   );
 }
