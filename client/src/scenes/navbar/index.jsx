5e429e734a683894f0ae1bfed7380a3b4bcea439
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-30 18:51:01 +0530

    modified index file and navbar component

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Art United
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
